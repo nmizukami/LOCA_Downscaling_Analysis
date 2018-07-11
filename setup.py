25114d4c2c13a9471a20bbe462c22e950ef071a0
Naoki Mizukami <mizukami@ucar.edu>
2018-07-11 14:00:08 -0600

    changed loca to bcsd in setpu.py

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ from setuptools import setup
 
 long_description = open('README.md').read() if exists('README.md') else ''
 
-setup(name='loca',
+setup(name='bcsd',
       version='0.0.1',
       description='',
-      url='https://github.com/jhamman/LOCA_Downscaling_Analysis',
+      url='https://github.com/nmizukami/LOCA_Downscaling_Analysis',
       license='Apache',
-      packages=['loca'],
+      packages=['bcsd'],
       long_description=long_description,
       zip_safe=False)
