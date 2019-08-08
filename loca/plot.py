7f6a213e409e3e459b7c9229e68868c274ee0f24
julievano <jvano@cheyenne1.ib0.cheyenne.ucar.edu>
2019-08-08 16:41:03 -0600

    two modifications:  1.  option to work on caspar jupyter hub, 2. making function to improve graphs

---
+++ loca/plot.py
@@ -0,0 +1,33 @@
+import cartopy
+import cartopy.crs as ccrs
+
+# to standardize plots
+PROJECTION = ccrs.Mercator()
+
+
+def make_plot(da, ax=None, plot_kwargs={}):
+
+    if ax is None:
+        ax = plt.gca()
+    
+    # make the actual plot
+    da.plot.pcolormesh(ax=ax, transform=ccrs.PlateCarree(), **plot_kwargs)
+    
+    # geometries
+    ax.coastlines();
+    ax.add_feature(cartopy.feature.OCEAN, facecolor='lightgrey')
+    ax.add_feature(cartopy.feature.BORDERS)
+    
+    # grid lines
+#     gl = ax.gridlines(draw_labels=True, linewidth=0)
+#     gl.xlabels_top = False
+#     gl.ylabels_left = False
+    
+    # boundaries
+    states_provinces = cartopy.feature.NaturalEarthFeature(
+        category='cultural',
+        name='admin_1_states_provinces_lines',
+        scale='50m',
+        facecolor='none')
+    ax.add_feature(states_provinces, edgecolor='gray')
+    return ax
